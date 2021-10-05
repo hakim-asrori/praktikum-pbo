bd4ffd8ade47618804fa09c0b50b43ff6de277b0
hakim-asrori <77718626+hakim-asrori@users.noreply.github.com>
2021-10-05 11:52:28 +0700

    Week 3 - Delete week didalam week

--- Week 3/Week 3/exception/ContohNumberFormatException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class ContohNumberFormatException {
-    public static void main(String[] args) {
-        int nilai = Integer.parseInt("abc");
-
-        System.out.println("Nilai = " + nilai);
-    }
-}
--- Week 3/Week 3/perulangan/PerulanganFor.java
+++
@@ -1,9 +0,0 @@
-package perulangan;
-
-public class PerulanganFor {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Iterasi ke-" + i);
-        }
-    }
-}
